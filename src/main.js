f5c5cc911e29505dcb7bd85506fe8217c1cb5c69
Ishkur10 <iliasouazani@gmail.com>
2022-03-31 13:23:03 +0200

    add .js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router/index";
+import router from "./router/index.js";
 import 'bootstrap/dist/css/bootstrap.min.css'
 const pinia = createPinia();
 
